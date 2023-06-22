8808dda61958cad946d0bda70ee18191705d0825
Patrick Gunderson <pat@theorigin.net>
2023-06-22 10:33:16 -0700

    added sense8

--- src/js/scroll-page.js
+++ src/js/scroll-page.js
@@ -5,46 +5,45 @@ videoNodes.forEach(element => videoList.push(element));
 let prevActiveElement;
 let isClicked = false;
 
-function mediaBlock(element){
+function mediaBlock(element) {
     return this;
 }
 
-function pauseAllVideos(){
+function pauseAllVideos() {
     videoList.forEach(vid => {
         vid.pause();
     });
 }
 
-function loadAndPlay(element){
+function loadAndPlay(element) {
     element.src = element.dataset.source;
     element.setAttribute("autoplay", true);
     element.play();
 }
 
-function getElementNearestActiveLocation(activeLocation){
+function getElementNearestActiveLocation(activeLocation) {
 
     //TODO: move evaluation point to center of video node instead of top
-    let leaderMid = videoList[0].getBoundingClientRect().top + (videoList[0].parentElement.offsetHeight  / 2)
+    let leaderMid = videoList[0].getBoundingClientRect().top + (videoList[0].parentElement.offsetHeight / 2)
     let leaderTopDelta = Math.abs(activeLocation - leaderMid);
-    let nearestElement = videoList.reduce((leader, vid)=>{
-        let vidMid = vid.getBoundingClientRect().top + (vid.parentElement.offsetHeight  / 2);
-        console.log(vidMid)
+    let nearestElement = videoList.reduce((leader, vid) => {
+        let vidMid = vid.getBoundingClientRect().top + (vid.parentElement.offsetHeight / 2);
         let vidTopDelta = Math.abs(activeLocation - vidMid);
-        if (vidTopDelta <= leaderTopDelta){
+        if (vidTopDelta <= leaderTopDelta) {
             leaderTopDelta = vidTopDelta;
             return vid;
         }
-        return  leader;
+        return leader;
     }, videoList[0]);
 
     return nearestElement;
 }
 
-function onScroll(){
+function onScroll() {
     // this is inelegant b/c when you click through from a different page, the browser counts that as a page interaction
     if (!isClicked) return;
     let activeElement = getElementNearestActiveLocation(0.4 * window.innerHeight);
-    if (activeElement != prevActiveElement){
+    if (activeElement != prevActiveElement) {
         prevActiveElement = activeElement;
         pauseAllVideos();
         loadAndPlay(activeElement);
@@ -63,38 +62,38 @@ window.addEventListener("touchstart", () => {
 let videoElements = document.querySelectorAll('video');
 let overlayElements = document.querySelectorAll('.play-button-overlay');
 
-videoElements.forEach(videoElement =>{
+videoElements.forEach(videoElement => {
     videoElement.addEventListener('play', onPlay);
     videoElement.addEventListener('pause', onPause);
     videoElement.addEventListener('click', onClickVideo);
 });
-overlayElements.forEach(overlayElement =>{
+overlayElements.forEach(overlayElement => {
     overlayElement.addEventListener('click', onClickOverlay);
 })
 
-function onPlay(e){
-    e.target.parentElement.querySelector('.play-button-overlay').style.display = "none";
+function onPlay(e) {
+    e.currentTarget.parentElement.querySelector('.play-button-overlay').style.display = "none";
 }
 
-function onPause(e){
-    e.target.parentElement.querySelector('.play-button-overlay').style.display = "none";
+function onPause(e) {
+    e.currentTarget.parentElement.querySelector('.play-button-overlay').style.display = "none";
 }
 
-function onClickOverlay(e){
+function onClickOverlay(e) {
     e.stopPropagation();
     e.preventDefault();
-    let videoElement = e.target.parentElement.querySelector('video');
+    let videoElement = e.currentTarget.parentElement.querySelector('video');
     loadAndPlay(videoElement);
-    overlayElements.forEach(overlayElement =>{
+    overlayElements.forEach(overlayElement => {
         overlayElement.style.display = "none";
     })
 }
 
-function onClickVideo(e){
-    videoEleemtns.forEach(videoElement =>{
+function onClickVideo(e) {
+    videoEleemtns.forEach(videoElement => {
         videoElement.pause();
     });
-    
+
 }
 
 
